98b971dae6fa711cde527c1361840de741aa7e8f
Jin3004 <jinjin30004@gmail.com>
2020-01-13 18:04:01 +0900

    コードのひな型を書いた

---
+++ Definition.hpp
@@ -0,0 +1,18 @@
+#pragma once
+#include <Siv3D.hpp>
+#include <memory>
+
+
+class Game {
+public:
+	const Font debug{ 20 }; //デバッグ用のフォントを用意
+	virtual void update(std::unique_ptr<Game>&){}
+	virtual void draw(){}
+};
+
+class SelectStage: public Game {
+public:
+	SelectStage(){}
+	void update(std::unique_ptr<Game>&) override;
+	void draw() override;
+};
