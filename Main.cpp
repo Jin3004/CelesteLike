671b778615d65b26732485fb47b6ef329c8fd0b4
Jin3004 <jinjin30004@gmail.com>
2020-01-25 14:47:05 +0900

    ｊ；ｌｊｆ

--- Main.cpp
+++ Main.cpp
@@ -2,26 +2,33 @@
 
 void Main() {
 
-	Scene::SetBackground(ColorF(0.8, 0.9, 1.0));
-	//背景を水色に変更
+	//Scene::SetBackground(ColorF(0.8, 0.9, 1.0));
+	////背景を水色に変更
+	//
+	//std::unique_ptr<Game> ptr = std::make_unique<SelectStage>();
+	//
+	//
+	//while (System::Update()) { //メインループ
+	//
+	//	ptr->update(ptr);
+	//
+	//	ptr->draw();
+	//
+	//}
 
-	std::unique_ptr<Game> ptr = std::make_unique<SelectStage>();
-
-
-	while (System::Update()) { //メインループ
-
-		ptr->update(ptr);
-
-		ptr->draw();
-
+	const FilePath path = FileSystem::FullPath(U"gui/selectstage1.toml");
+	const Texture chip(U"img/block.png");
+	SimpleGUIManager gui(path);
+	while (System::Update()) {
+		chip.draw(40, 40);
+		gui.draw();
 	}
+
 
 }
 
 void SelectStage::update(std::unique_ptr<Game>& ptr) {
-	++i;
 }
 
 void SelectStage::draw() {
-	Rect(i, 20, 100, 200).draw(Palette::Red);
 }
